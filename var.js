68456458df92bc20e71fa8cf4c78c486aa7dce22
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-11-04 15:27:10 +0100

    4-Oct-2024

---
+++ var.js
@@ -0,0 +1,38 @@
+// javaScript is a dynamic programming language which is light weight in nature.
+// comment out -- ctrl + /
+// framework and libraries
+// React.js, Vue.js, Angular, Nextjs, electron, Nodejs, Nestjs,
+
+// JavaScript syntax and variables
+// rules to be followed 
+
+// functions, variables, objects, properties, method, 
+
+// variable == Vary == variation
+// declared the variables x and y
+// var x;
+// var y;
+
+// x = 10;
+// y = 15;
+
+var x = 10; 
+var y = 15;
+var res;
+
+// name of the function is add
+function add(x, y) {
+    return x + y;
+}
+
+// Call the function and assign the result to res
+var res = add(x, y);
+
+console.log(res); // This will output 25
+
+
+
+
+
+
+
